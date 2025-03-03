13004659231110833aff5ad497397dd2355139f9
jojo2283 <denisbermas04@mail.ru>
2025-03-03 18:59:00 +0300

    update

--- src/main/java/org/example/task1.java
+++ src/main/java/org/example/task1.java
@@ -3,17 +3,18 @@ package org.example;
 public class Task1 {
 
     public static double calc_sin(int n, double x) {
+        double normX = x % (2 * Math.PI);
         double result = 0;
         int k = 1;
         int c = 1;
-        double tempX = x;
+        double tempX = normX;
         long factorial = 1;
         for (int i = 1; i <= n; i++) {
             result += (c * tempX) / (factorial);
             c = c * (-1);
             k += 2;
             factorial *= k * (k - 1);
-            tempX = tempX * x * x;
+            tempX = tempX *normX * normX;
         }
         return result;
     }
