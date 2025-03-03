0ab9c659b056d02b561b1336d7f4bbbf7f54586c
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-03-03 18:20:48 +0300

    маленький тест

--- src/test/java/SinTest.java
+++ src/test/java/SinTest.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -9,12 +11,37 @@ import org.example.task1;
 
 
 public class SinTest {
-    @ParameterizedTest(name="lalala")
-    @DisplayName("Check from Math.Sin")
-    @ValueSource(doubles = {0})
-    void checkMathSin(double param) {
+
+    private static final int TERMS_COUNT = 1000;
+    private static final double EPSILON = 0.01;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Запускаем тестики на синус...");
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("Тест на соответствие с Math.sin()")
+    @ValueSource(doubles = {0, 1})
+    void checkMathSin(double input) {
+        double result =  task1.calc_sin(TERMS_COUNT, input);
         assertAll(
-                ()-> assertEquals(Math.sin(param), task1.calc_sin(10, param))
+                ()-> assertEquals(Math.sin(input), result, EPSILON)
         );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на табличные значения")
+    @CsvSource({
+            "0, 0",
+            "1.57079632679, 1",
+            "3.14159265359, 0",
+            "4.71238898038, -1",
+            "6.28318530718, 0"
+    })
+    void checkTableValues(double input, double expected) {
+        double result =  task1.calc_sin(TERMS_COUNT, input);
+        assertEquals(expected, result, EPSILON);
     }
 }
