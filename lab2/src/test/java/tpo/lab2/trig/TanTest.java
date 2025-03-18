f8315abbb970bfa62f7197d6ee1f798972d7ff80
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-03-19 00:37:23 +0300

    тесты на тангенс

--- lab2/src/test/java/tpo/lab2/trig/TanTest.java
+++ lab2/src/test/java/tpo/lab2/trig/TanTest.java
@@ -10,8 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,7 +26,7 @@ public class TanTest {
     private Cos cosMock;
 
     private Tan tan;
-
+    private Tan mockTan;
     @BeforeAll
     static void info() {
         System.out.println("Запускаем тесты на тангенс...");
@@ -35,13 +34,14 @@ public class TanTest {
 
     @BeforeEach
     void setUp() {
-        tan = new Tan(sinMock, cosMock);
+        mockTan = new Tan(sinMock, cosMock);
+        tan = new Tan(new Sin(), new Cos(new Sin()));
     }
 
     @ParameterizedTest
     @DisplayName("Тест на соответствие с Math.sin(x) / Math.cos(x)")
     @ValueSource(doubles = {0, 1, 0.5, 0.3, 0.2, 0.6, -1, -0.6, -0.4, -0.3})
-    void checkForMathTan(double input) {
+    void checkForMathTanMock(double input) {
         double sinX = Math.sin(input);
         double cosX = Math.cos(input);
 
@@ -50,20 +50,19 @@ public class TanTest {
         when(cosMock.compute(input, EPSILON)).thenReturn(cosX);
 
 
-        double result = tan.compute(input, EPSILON);
+        double result = mockTan.compute(input, EPSILON);
         assertEquals(sinX / cosX, result, DELTA);
     }
 
     @ParameterizedTest
     @DisplayName("Тест на исключение при cos(x) = 0")
     @ValueSource(doubles = {Math.PI / 2, 3 * Math.PI / 2})
-    void checkTanExceptionOnZeroCos(double input) {
+    void checkTanExceptionOnZeroCosMock(double input) {
 
         when(sinMock.compute(input, EPSILON)).thenReturn(Math.sin(input));
         when(cosMock.compute(input, EPSILON)).thenReturn(0.0);
 
-
-        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> tan.compute(input, EPSILON));
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> mockTan.compute(input, EPSILON));
         assertEquals("tan(x) не определён при cos(x) = 0", exception.getMessage());
     }
 
@@ -71,12 +70,12 @@ public class TanTest {
     @DisplayName("Тест на табличные значения с Math.sin(x) / Math.cos(x)")
     @CsvSource({
             "0, 0.0",
-            "0.7853981633974483, 1.0",  // Math.PI / 4 = 0.7853981633974483
-            "1.5707963267948966, Infinity",  // Math.PI / 2 = 1.5707963267948966
-            "2.356194490192345, -1.0",  // 3 * Math.PI / 4 = 2.356194490192345
-            "3.141592653589793, 0.0"  // Math.PI = 3.141592653589793
+            "0.7853981633974483, 1.0",
+            "1.5707963267948966, Infinity",
+            "2.356194490192345, -1.0",
+            "3.141592653589793, 0.0"
     })
-    void checkTableValues(double input, double expected) {
+    void checkTableValuesMock(double input, double expected) {
         double sinX = Math.sin(input);
         double cosX = Math.cos(input);
 
@@ -84,11 +83,63 @@ public class TanTest {
         when(cosMock.compute(input, EPSILON)).thenReturn(cosX);
 
         if (Math.abs(cosX) > EPSILON) {
-            double result = tan.compute(input, EPSILON);
+            double result = mockTan.compute(input, EPSILON);
             assertEquals(expected, result, DELTA);
         } else {
-            ArithmeticException exception = assertThrows(ArithmeticException.class, () -> tan.compute(input, EPSILON));
+            ArithmeticException exception = assertThrows(ArithmeticException.class, () -> mockTan.compute(input, EPSILON));
             assertEquals("tan(x) не определён при cos(x) = 0", exception.getMessage());
         }
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на соответствие с Math.tan()")
+    @ValueSource(doubles = {0, Math.PI / 4, Math.PI, 2 * Math.PI})
+    void checkForMathTan(double input) {
+        double result = tan.compute(input, EPSILON);
+        assertAll(
+                () -> assertEquals(Math.tan(input), result, EPSILON)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на бесконечность (для x = π/2, 3π/2, и т.д.)")
+    @ValueSource(doubles = {Math.PI / 2, 3 * Math.PI / 2})
+    void checkInfinity(double input) {
+        assertThrows(ArithmeticException.class, () -> tan.compute(input, EPSILON));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на нули (для углов кратных π)")
+    @ValueSource(doubles = {0, Math.PI, 2 * Math.PI, 3 * Math.PI})
+    void checkZeroes(double input) {
+        double result = tan.compute(input, EPSILON);
+        assertAll(
+                () -> assertEquals(0.0, result, EPSILON)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на табличные значения с Math.tan()")
+    @ValueSource(doubles = {0, Math.PI / 4, Math.PI / 3, Math.PI / 6})
+    void checkMathTanTable(double input) {
+        double result = tan.compute(input, EPSILON);
+        assertAll(
+                () -> assertEquals(Math.tan(input), result, EPSILON)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест на табличные значения (высчитано с помощью супер-точной ЭВМ)")
+    @CsvSource({
+            "0,0.0",
+            "0.5235987756,0.5773502692",
+            "0.7853981634,1.0",
+            "1.0471975512,1.7320508076",
+            "2.61799387799,-0.5773502692",
+            "3.1415926536,0.0"
+    })
+    void checkTableValues(double input, double expected) {
+        double result = tan.compute(input, EPSILON);
+        assertEquals(expected, result, EPSILON);
     }
 }
