d964ed40d2eb556145b28b73e6637ccdc7bace01
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-04-01 16:39:56 +0300

    тест на конфигурацию прокси и itunes тегов

--- lab3/tests/proxy.spec.ts
+++ lab3/tests/proxy.spec.ts
@@ -14,9 +14,9 @@ test.describe("Операции с proxy", () => {
     IT_EMAIL: "itunes_email",
     IT_AUTHOR: "itunes_author",
     IT_COPYRIGHT: "itunes_copywright",
-    FEED_TITLE: "",
-    FEED_NEW_TITLE: "",
-    FEED_CUSTOM_FIELD_URL: "",
+    FEED_TITLE: "vcru-rss-dnrvc8m3",
+    FEED_NEW_TITLE: "vcru-rss-dnrvc8m3_",
+    FEED_CUSTOM_FIELD_URL: "vcru-rss-v0p5nh5el7o",
   };
   test.beforeAll(async () => {
     TEST_DATA.FEED_TITLE = `vcru-rss-${Math.random()
@@ -29,6 +29,7 @@ test.describe("Операции с proxy", () => {
       .toString(36)
       .substring(2, 22)}`;
   });
+
   test("Можно создать Proxy", async ({ page }) => {
     await page.goto("/");
 
@@ -211,5 +212,246 @@ test.describe("Операции с proxy", () => {
     const download = await downloadPromise;
 
     expect(download).toBeTruthy();
+  });
+
+  test("Можно сконфигурировать прокси", async ({ page }) => {
+    page.goto("/");
+    await page
+      .locator(
+        `xpath=//div[contains(@class, 'proxy-list-item')]//*[contains(text(), "${TEST_DATA.FEED_TITLE}")]/following::button[1]`
+      )
+      .nth(0)
+      .click(); // Кликаем на Configure Proxy
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/mat-form-field[1]/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.FEED_NEW_TITLE); // Изменяем Proxy Title
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/div[1]/mat-form-field/div/div[1]/div[3]/custom-url-input/div/input"
+      )
+      .fill(TEST_DATA.FEED_CUSTOM_FIELD_URL); // Изменяем Custom Feed URL
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/fieldset/mat-form-field[1]/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.CUSTOM_FIELD_TITLE); // Изменяем Custom Feed Title
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/fieldset/mat-form-field[2]/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.CUSTOM_FIELD_DESC); // Изменяем Custom Feed Description
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/fieldset/div[1]/mat-form-field/div/div[1]/div[3]"
+      )
+      .click(); // Открываем селект podcast enclosure link
+
+    await page
+      .locator("xpath=/html/body/div[2]/div[2]/div/div/div/mat-option[1]/span")
+      .click(); // Выбираем any rich media file
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/fieldset/div[2]/mat-checkbox/label/span[1]"
+      )
+      .click(); // Ставим чекбокс индексации поисковыми системами
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/div/button[2]"
+      )
+      .click(); // Сохраняем изменения
+
+    await page.waitForLoadState("networkidle"); // Ждем загрузку
+
+    await page.goto(
+      `https://feeds.feedburner.com/${TEST_DATA.FEED_CUSTOM_FIELD_URL}`
+    );
+
+    await expect(page.locator("xpath=/rss/channel/title")).toHaveText(
+      TEST_DATA.CUSTOM_FIELD_TITLE
+    );
+    await expect(page.locator("xpath=/rss/channel/description")).toHaveText(
+      TEST_DATA.CUSTOM_FIELD_DESC
+    );
+    await expect(page.locator("xpath=/rss/channel/description")).toHaveText(
+      TEST_DATA.CUSTOM_FIELD_DESC
+    );
+
+    await expect(page.locator("xpath=//enclosure")).not.toHaveCount(0);
+
+    await expect(
+      page.locator(
+        '//*[local-name() = "meta" and @name = "robots" and @content = "noindex"]'
+      )
+    ).not.toHaveCount(0); // Проверяем что бы был noindex
+  });
+
+  test("Можно сконфигурировать iTunes Tags", async ({ page }) => {
+    await page.goto("/");
+
+    await page
+      .locator(
+        `xpath=//div[contains(@class, 'proxy-list-item')]//*[contains(text(), "${TEST_DATA.FEED_NEW_TITLE}")]/following::button[1]`
+      )
+      .nth(0)
+      .click(); // Кликаем на Configure Proxy
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/form/div[2]/button"
+      )
+      .click(); // Кликаем сконфигурировать iTunes Tags
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[1]/mat-form-field/div/div[1]/div/categories-selector/form/mat-form-field[1]/div/div[1]/div[3]"
+      )
+
+      .click(); // Селект категории
+
+    await page
+      .locator(
+        `xpath=//span[contains(@class, 'mat-option-text') and contains(text(), '${TEST_DATA.IT_CATEGORY}')]`
+      )
+      .nth(0)
+      .click(); // Выбор опции категории
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[1]/mat-form-field/div/div[1]/div/categories-selector/form/mat-form-field[2]/div/div[1]/div[3]"
+      )
+
+      .click(); // Селект подкатегории
+
+    await page
+      .locator(
+        `xpath=//span[contains(@class, 'mat-option-text') and contains(text(), '${TEST_DATA.IT_SUBCATEGORY}')]`
+      )
+      .nth(0)
+      .click(); // Выбор опции категории
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[2]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_IMAGE);
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[3]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_SUBTITLE);
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[4]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_SUMMARY);
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[5]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_KEYWORDS);
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[6]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_EMAIL);
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[1]/div[6]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_EMAIL);
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[2]/div[1]/mat-form-field/div/div[1]/div[3]"
+      )
+      .click(); // Выбор explicit content
+    await page
+      .locator("xpath=/html/body/div[2]/div[2]/div/div/div/mat-option[2]/span")
+      .click();
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[2]/div[2]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_AUTHOR);
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/form/fieldset[2]/div[3]/mat-form-field/div/div[1]/div[3]/input"
+      )
+      .fill(TEST_DATA.IT_COPYRIGHT);
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/enclosure-transformation/div/div/button[3]"
+      )
+      .click();
+
+    await page.waitForLoadState("networkidle");
+
+    await page
+      .locator(
+        "xpath=/html/body/app-root/div/div[2]/div/manage-proxy/div/div/button[1]"
+      )
+      .click();
+
+    await page.waitForLoadState("networkidle"); // Ждем загрузку
+
+    await page.goto(
+      `https://feeds.feedburner.com/${TEST_DATA.FEED_CUSTOM_FIELD_URL}`
+    );
+
+    await page.waitForLoadState("networkidle"); // Ждем загрузку
+
+    await expect(
+      page.locator(
+        `//*[local-name() = "category" and @text = "${TEST_DATA.IT_CATEGORY}"]`
+      )
+    ).not.toHaveCount(0);
+
+    await expect(
+      page.locator(
+        `//*[local-name() = "category" and @text = "${TEST_DATA.IT_SUBCATEGORY}"]`
+      )
+    ).not.toHaveCount(0);
+
+    await expect(
+      page.locator(
+        `//*[local-name() = "image" and @href = "${TEST_DATA.IT_IMAGE}"]`
+      )
+    ).not.toHaveCount(0);
+
+    await expect(
+      page.locator("//*[local-name() = 'subtitle']").nth(0)
+    ).toHaveText(TEST_DATA.IT_SUBTITLE);
+    await expect(
+      page.locator("//*[local-name() = 'summary']").nth(0)
+    ).toHaveText(TEST_DATA.IT_SUMMARY);
+    await expect(
+      page.locator("//*[local-name() = 'keywords']").nth(0)
+    ).toHaveText(TEST_DATA.IT_KEYWORDS);
+
+    await expect(
+      page.locator("//*[local-name() = 'author']").nth(0)
+    ).toHaveText(TEST_DATA.IT_AUTHOR);
+    await expect(page.locator("//*[local-name() = 'email']").nth(0)).toHaveText(
+      TEST_DATA.IT_EMAIL
+    );
+    await expect(
+      page.locator("//*[local-name() = 'explicit']").nth(0)
+    ).toHaveText("yes");
+
+    await expect(
+      page.locator("//*[local-name() = 'copyright']").nth(0)
+    ).toHaveText(TEST_DATA.IT_COPYRIGHT);
   });
 });
